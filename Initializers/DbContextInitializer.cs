53d9451e6c262cd6edc0b8c95e4f23a7c486644f
Goluboy <vaaalera12321@gmail.com>
2024-12-22 23:48:26 +0500

    Initial commit

---
+++ Initializers/DbContextInitializer.cs
@@ -0,0 +1,109 @@
+﻿using CSharpClicker.Web.Domain;
+using CSharpClicker.Web.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpClicker.Web.Initializers;
+
+public static class DbContextInitializer
+{
+    public static void AddAppDbContext(IServiceCollection services)
+    {
+        var pathToDbFile = GetPathToDbFile();
+        services
+            .AddDbContext<AppDbContext>(options => options
+                .UseSqlite($"Data Source={pathToDbFile}"));
+
+        string GetPathToDbFile()
+        {
+            var applicationFolder = Path.Combine(Environment.GetFolderPath(
+                Environment.SpecialFolder.LocalApplicationData), "CSharpClicker");
+
+            if (!Directory.Exists(applicationFolder))
+            {
+                Directory.CreateDirectory(applicationFolder);
+            }
+
+            return Path.Combine(applicationFolder, "CSharpClicker.db");
+        }
+    }
+
+    public static void InitializeDbContext(AppDbContext appDbContext)
+    {
+        const string Boost1 = "Laezel";
+        const string Boost2 = "Wyll";
+        const string Boost3 = "Gale";
+        const string Boost4 = "Shadowheart";
+        const string Boost5 = "Astarion";
+        const string Boost6 = "Karlach";
+        const string Boost7 = "The_Dark_Urge";
+
+        appDbContext.Database.Migrate();
+
+        var existingBoosts = appDbContext.Boosts
+            .ToArray();
+
+        AddBoostIfNotExist(Boost1, price: 100, profit: 1);
+        AddBoostIfNotExist(Boost2, price: 300, profit: 5, true);
+        AddBoostIfNotExist(Boost3, price: 1000, profit: 15, true);
+        AddBoostIfNotExist(Boost4, price: 5000, profit: 20);
+        AddBoostIfNotExist(Boost5, price: 60000, profit: 250);
+        AddBoostIfNotExist(Boost6, price: 200000, profit: 1000);
+        AddBoostIfNotExist(Boost7, price: 10000000, profit: 30000, true);
+
+        AddRandomUsers();
+
+        appDbContext.SaveChanges();
+
+        void AddRandomUsers()
+        {
+            return;
+
+            const int limit = 130000534;
+            const int asciLimit = 126;
+            const int symbolsLimit = 15;
+            const int symbolsStart = 5;
+
+            var random = new Random();
+
+            for (var i = 0; i < 200; i++)
+            {
+                var score = random.Next(limit);
+                var symbolsCount = random.Next(symbolsStart, symbolsLimit);
+
+                var userName = string.Empty;
+                for (var j = 0; j < symbolsCount; j++)
+                {
+                    var character = random.Next(asciLimit);
+                    userName += char.ConvertFromUtf32(character);
+                }
+
+                appDbContext.Users.Add(new ApplicationUser
+                {
+                    UserName = userName,
+                    RecordScore = score,
+                });
+            }
+        }
+
+        void AddBoostIfNotExist(string name, long price, long profit, bool isAuto = false)
+        {
+            if (!existingBoosts.Any(eb => eb.Title == name))
+            {
+                var pathToImage = Path.Combine(".", "Resources", "BoostImages", $"{name}.png");
+                using var fileStream = File.OpenRead(pathToImage);
+                using var memoryStream = new MemoryStream();
+
+                fileStream.CopyTo(memoryStream);
+
+                appDbContext.Boosts.Add(new Boost
+                {
+                    Title = name,
+                    Price = price,
+                    Profit = profit,
+                    IsAuto = isAuto,
+                    Image = memoryStream.ToArray(),
+                });
+            }
+        }
+    }
+}
---
+++ UseCases/AddPoints/AddPointsCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using CSharpClicker.Web.DomainServices;
+using CSharpClicker.Web.Infrastructure.Abstractions;
+using CSharpClicker.Web.UseCases.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpClicker.Web.UseCases.AddPoints;
+
+public class AddPointsCommandHandler : IRequestHandler<AddPointsCommand, ScoreDto>
+{
+    private readonly ICurrentUserAccessor currentUserAccessor;
+    private readonly IAppDbContext appDbContext;
+
+    public AddPointsCommandHandler(ICurrentUserAccessor currentUserAccessor, IAppDbContext appDbContext)
+    {
+        this.currentUserAccessor = currentUserAccessor;
+        this.appDbContext = appDbContext;
+    }
+
+    public async Task<ScoreDto> Handle(AddPointsCommand request, CancellationToken cancellationToken)
+    {
+        var userId = currentUserAccessor.GetCurrentUserId();
+        var user = await appDbContext.ApplicationUsers
+            .Include(user => user.UserBoosts)
+            .ThenInclude(ub => ub.Boost)
+            .FirstAsync(user => user.Id == userId);
+
+        var profitPerSecond = user.UserBoosts.GetProfit(shouldCalculateAutoBoosts: true);
+        var profitPerClick = user.UserBoosts.GetProfit();
+
+        long autoPoints = ((DateTime.UtcNow - user.LastOnline).Seconds > 0) 
+            ? profitPerSecond * ((DateTime.UtcNow - user.LastOnline).Seconds) 
+            : 0;
+
+        var clickedPoints = profitPerClick * request.Clicks;
+
+        user.CurrentScore += autoPoints + clickedPoints;
+        user.RecordScore += autoPoints + clickedPoints;
+        user.LastOnline = DateTime.UtcNow;
+
+        await appDbContext.SaveChangesAsync();
+
+        return new ScoreDto
+        {
+            CurrentScore = user.CurrentScore,
+            RecordScore = user.RecordScore,
+            ProfitPerClick = profitPerClick,
+            ProfitPerSecond = profitPerSecond,
+        };
+    }
+}
